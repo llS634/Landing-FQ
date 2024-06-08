d0329ebc4d5f7ce26c7192209dc81e53609c7216
Liz <eliza.sashenkova@gmail.com>
2024-06-08 16:39:49 +0700

    fix: image replacement and adaptive adjustment
    1. Replace image format with svg
    2. Small changes adaptive
    3. Add hid the topic button when scroll down

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ function toggleTheme() {
         themeIcon.setAttribute('src', 'assets/svg/header_block/color-theme-dark.svg');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.get-app-button, .get-app-mini');
+    const popup = document.getElementById('popup');
+    const close = document.getElementById('close');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            popup.style.display = 'block';
+        });
+    });
+
+    close.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeSwitcher = document.querySelector('.theme-switcher');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            themeSwitcher.style.display = 'none';
+        } else {
+            themeSwitcher.style.display = 'block';
+        }
+    });
+});
